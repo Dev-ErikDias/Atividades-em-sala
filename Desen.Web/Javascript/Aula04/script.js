7ebe38bda1c3edc042ef428c8fe5d831e392330c
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2023-10-05 11:49:56 -0300

    Create script.js

---
+++ Desen.Web/Javascript/Aula04/script.js
@@ -0,0 +1,5 @@
+function receberFoco(campo){
+          label = document.getElementById("resposta");
+          label.innerHTML = campo.name + " perdeu foco";
+          //onblur é perder o foco, onfocus é ganhar o foco
+}
