4e88f3efd297cd49e3587c843867936855a00710
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2023-11-01 18:58:48 -0300

    Update script.js

--- Desen.Web/Javascript/Aula05/script.js
+++ Desen.Web/Javascript/Aula05/script.js
@@ -4,6 +4,9 @@ const robo = document.createElement("img");
 const casa = document.createElement("img");
 const col=8,lin=8;
 
+casa.setAttribute('class', "casa");
+robo.setAttribute('class', "robo");
+
 function criarTabela(){
    var pai=document.getElementsByTagName('body')[0];
    var tabela=document.createElement('table');
@@ -42,6 +45,7 @@ function posicionarImagens(){
        botao.src = "img/"+vet[i];
        celula.appendChild(botao);
    }
+
      lR= Math.round(Math.random() * (lin/2));
      cR= Math.round(Math.random()* (col-1));
      posRobo=document.getElementById(lR+","+cR);
@@ -55,68 +59,89 @@ function posicionarImagens(){
 }
 
 function incluirComandos(bt){
-    celula = document.getElementById('comandos');
-    var i = parseInt(bt.id);
+  celula = document.getElementById('comandos');
+  var i = parseInt(bt.id);
 
-    switch(i){
-        case 0:{
-            celula.innerHTML += 'Abaixo<br>';
-            vetComandos.push(i);
-            break;
-        }
-
-        case 1:{
-            celula.innerHTML += 'Acima<br>';
-            vetComandos.push(i);
-            break;
-        }
-
-        case 2:{
-            celula.innerHTML += 'Direita<br>';
-            vetComandos.push(i);
-            break;
-        }
-
-        case 3:{
-            celula.innerHTML += 'Esquerda<br>';
-            vetComandos.push(i);
-            break;
-        }
-
-        case 4:{
-            executarComandos();
-            break;
-        }
+  switch(i){
+    case 0:{
+      celula.innerHTML += 'Abaixo<br>';
+      vetComandos.push(i);
+      break;
     }
-}
 
+    case 1:{
+      celula.innerHTML += 'Acima<br>';
+      vetComandos.push(i);
+      break;
+    }
+
+    case 2:{
+      celula.innerHTML += 'Direita<br>';
+      vetComandos.push(i);
+      break;
+    }
+
+    case 3:{
+      celula.innerHTML += 'Esquerda<br>';
+      vetComandos.push(i);
+      break;
+    }
+
+    case 4: {
+      for (let i = 1; i <= vetComandos.length; i++) {
+        setTimeout(executarComandos, i * 500);
+      }
+      break;
+    }    
+  }
+}
+  
 function executarComandos(){
-    var i = vetComandos[comando++];
-    switch(i){
-        case 0:{
-            movimentarRobo(++lR, cR);
-            break;
-        }
-
-        case 1:{
-            movimentarRobo(--lR, cR);
-            break;
-        }
-
-        case 2:{
-            movimentarRobo(lR, ++cR);
-            break;
-        }
-
-        case 3:{
-            movimentarRobo(lR, --cR);
-            break;
-        }
+  var i = vetComandos[comando++];
+  switch(i){
+    case 0:{
+      movimentarRobo(++lR, cR);
+      break;
     }
+    
+    case 1:{
+      movimentarRobo(--lR, cR);
+      break;
+    }
+    
+    case 2:{
+      movimentarRobo(lR, ++cR);
+      break;
+    }
+    
+    case 3:{
+      movimentarRobo(lR, --cR);
+      break;
+    }
+  }
+}
+  
+function movimentarRobo(l, c){
+  if(posRobo != posCasa){
+    posRobo.innerHTML = "";
+  }
+  posRobo = document.getElementById(l+","+c);
+  verificarVitoria(comando);
+  posRobo.appendChild(robo);   
 }
 
-function movimentarRobo(l, c){
-    posRobo.innerHTML = "";
-    posRobo = document.getElementById(l+","+c);
-    posRobo.appendChild(robo);   
+function verificarVitoria(comandoVez){
+  var status = document.getElementById('status');
+  
+  if(posCasa == posRobo){
+    robo.style.width = "50px";
+    casa.style.width = "50px";
+    robo.style.height = "auto";
+    casa.style.height = "auto";
+  }else{
+    robo.style.width = "75px";
+    casa.style.width = "75px";
+    robo.style.height = "auto";
+    casa.style.height = "auto";
+  }  
 }
