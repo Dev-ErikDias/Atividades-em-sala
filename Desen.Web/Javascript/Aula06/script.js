346cc1b8d748d065bef7ab1007ef9ab52fc56963
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2023-11-16 09:58:18 -0300

    Update script.js

--- Desen.Web/Javascript/Aula06/script.js
+++ Desen.Web/Javascript/Aula06/script.js
@@ -4,17 +4,17 @@ var produtos = [
     [35, 50, 350]
 ];
 
-var pos;
-
+var pos; soma = 0;
+var pai;
 function buscarProduto(){
-    var cod = document.frVendas.cod.value;
+    var cod = document.frVendas.cod.value;      
     pos = produtos[0].indexOf(parseInt(cod));
     document.frVendas.produto.value = produtos[1][pos];
 }
 
 function incluir(){
     var qtd = parseInt(document.frVendas.qtd.value);
-    var pai = document.getElementById("tabela");
+    pai = document.getElementById("tabela");
     var tr = document.createElement("tr");
     for(let i = 0; i < 3; i++){
         var td = document.createElement("td");
@@ -23,6 +23,20 @@ function incluir(){
     }
     td = document.createElement("td");
     td.innerHTML = qtd * produtos[2][pos];
+    soma += qtd * produtos[2][pos];
+    tr.appendChild(td);
+    pai.appendChild(tr);
+    
+}
+
+function concluirVenda(){
+    tr = document.createElement("tr");
+    td = document.createElement("td");
+    td.setAttribute("colspan", 3);
+    td.innerHTML = "Total";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = soma;
     tr.appendChild(td);
     pai.appendChild(tr);
 }
