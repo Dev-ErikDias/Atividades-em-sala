33aab6389370141184b3c406c955387fd2e98e54
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2023-11-23 11:43:18 -0300

    Create script.js

---
+++ Desen.Web/Javascript/Aula07/canvas/script.js
@@ -0,0 +1,7 @@
+const largura = 400, altura = 400;
+var pai = document.getElementsByTagName("body")[0];
+var canvas = document.createElement("canvas");
+canvas.setAttribute("width", largura);
+canvas.setAttribute("height", altura);
+canvas.style = "background-color: #D020F0";
+pai.appendChild(canvas);
